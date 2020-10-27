976af941d1c1e399f1a4abc6982fd2281abdd6a6
Gabriel Fernando <gabr.fern99@gmail.com>
2020-10-27 08:46:12 -0300

    Add data and test content

---
+++ test/check/color.h
@@ -0,0 +1,4 @@
+#define GREEN "\033[0;32m"
+#define RED "\033[0;31m"
+#define YELLOW "\033[0;33m"
+#define DEFAULT "\033[0m"
---
+++ test/check/config.h
@@ -0,0 +1,53 @@
+/* Subunit protocol result output */
+#define ENABLE_SUBUNIT 0
+
+/* Define to 1 if you have the declaration of `localtime_r', and to 0 if you
+   don't. */
+#define HAVE_DECL_LOCALTIME_R 0
+
+/* Define to 1 if you have the declaration of `strdup', and to 0 if you don't.
+   */
+#define HAVE_DECL_STRDUP 1
+
+/* Define to 1 if you have the declaration of `strsignal', and to 0 if you
+   don't. */
+#define HAVE_DECL_STRSIGNAL 1
+
+/* Define to 1 if you have the `fork' function. */
+#define HAVE_FORK 1
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define to 1 if you have the `rt' library (-lrt). */
+#define HAVE_LIBRT 1
+
+/* Define to 1 if you have the `mkstemp' function. */
+#define HAVE_MKSTEMP 1
+
+/* Define if you have POSIX threads libraries and header files. */
+#define HAVE_PTHREAD 1
+
+/* Define to 1 if you have the `sigaction' function. */
+#define HAVE_SIGACTION 1
+
+/* Define to 1 if you have a C99 compliant 'snprintf' function. */
+#define HAVE_SNPRINTF 1
+
+/* Define to 1 if you have the <stdarg.h> header file. */
+#define HAVE_STDARG_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have <sys/wait.h> that is POSIX.1 compatible. */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have a C99 compliant 'vsnprintf' function. */
+#define HAVE_VSNPRINTF 1
